d547c36e8a5a38db051cb6d3c01402c2e2958e02
eliasearlym <eliasearlym@gmail.com>
2022-06-27 13:03:53 -0400

    fix: fix indentation

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    printWidth: 100,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: false,
-    backetSameLine: true,
-    singleAttributePerLine: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  backetSameLine: true,
+  singleAttributePerLine: true,
 }
